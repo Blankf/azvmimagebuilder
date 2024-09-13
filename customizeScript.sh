4f3168999e792cdf0d03dad1c6868d9679de5775
Ferry Blankendaal <ferry.blankendaal@gmail.com>
2024-09-13 16:50:05 +0200

    Update customizeScript.sh

--- customizeScript.sh
+++ customizeScript.sh
@@ -3,3 +3,10 @@
 mkdir /var/log/staging
 touch /var/log/staging/creativityAtItsBest.md
 echo Azure-Image-Builder-Was-Here  > /var/log/staging/azvmimagebuilder.log
+
+apt-get update
+apt-get install -y nginx
+
+# Enable and start Nginx service
+systemctl enable nginx
+systemctl start nginx
